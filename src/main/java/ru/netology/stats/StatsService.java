cededf09be76557a7148d991e209b18284394ef9
Paul Filatow <paulfltv@gmail.com>
2021-01-18 13:39:48 +0300

    Third_commit__Tests added

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,50 +2,131 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public int calculateSumTotal(int[] purchases) {
-        int sum = 0;
-        for (int purchase : purchases) {
-            sum += purchase;
-        }
-        return sum;
-    }
+	public int calculateSumTotal(int[] purchases) {
+		int sum = 0;
+		for (int purchase : purchases) {
+			sum += purchase;
+		}
+		return sum;
+	}
+//
+	//          иной вариант:
+//    public int sum(int[] purchases) {
+//        int sum = 0;
+//        for (int purchase : purchases) {
+//            sum += purchase;
+//        }
+//    }
 
-    public int calculateSumAverage(int[] purchases) {
-        int sum = 0;
-        int months = 12;
-        for (int purchase : purchases) {
-            sum += purchase;
-        }
-        return sum / months;
-    }
+	public int calculateSumAverage(int[] purchases) {
+		int sum = 0;
+		int months = 12;
+		for (int purchase : purchases) {
+			sum += purchase;
+		}
+		return sum / months;
+	}
 
-    public int findSumMax(int[] purchases) {
-        int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int month = 0;
-        for (int value: values) {
-            month ++;
-        }
-        int currentMax = purchases[0];
-        for (int purchase : purchases) {
-            if (currentMax <= purchase) {
-                currentMax = purchase;
-            }
-        }
-        return currentMax;
-    }
-    public int findSumMin(int[] purchases) {
-        int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int month = 0;
-        for (int value: values) {
-            month ++;
-        }
-        int currentMin = purchases[0];
-        for (int purchase : purchases) {
-            if (currentMin >= purchase) {
-                currentMin = purchase;
-            }
-        }
-        return currentMin;
-    }
+	//
+	//           иной вариант-1:
+//    public int sum(int[] purchases) {
+//        int sum = sum(purchases);
+//        return sum(purchases) / purchases.length;
+//    }
+	//           иной вариант-2:
+//    public int sum(int[] purchases) {
+//    return sum(purchases) / purchases.length;
+//    }
+//
+
+	public int numberMonthSumMax(int[] purchases) {
+		int sumMax = purchases[0];
+		for (int purchase : purchases) {
+			if (sumMax < purchase) {
+				sumMax = purchase;
+			}
+		}
+		int month = 0;
+		int numberMonthSumMax = 0;
+		for (int purchase : purchases) {
+			month++;
+			if (sumMax == purchase) {
+				numberMonthSumMax = month;
+			}
+		}
+		return numberMonthSumMax;
+	}
+	//                  иной вариант:
+//
+//  public int numberMonthSumMax(int[] purchases) {
+//  6 строк ниже (вкл. } и }), отвечающих за поиск максимальной суммы продаж, можно выделить и,
+//     int sumMax = purchases[0];
+//        for (int purchase : purchases) {
+//            if (purchase > sumMax) {
+//                sumMax = purchase;
+//            }
+//        }
+//    нажав комбинацию Ctrl + Alt + M,
+//    или, следуя последовательности действий ниже,
+//    выделить 6 строк -> правая кнопка на выделенном -> Refactor -> Extract Method...
+//    в появившемся окне выбрать нужнные значения (public / private и т.д.) и нажать Refactor, после чего
+//    появляется новый метод public int getSumMax(int[] purchases) {
+
+
+//        int month = 8;
+//        int numberMonthSumMax = 8;
+//        for (int purchase : purchases) {
+//            month++;
+// при этом month++; является сокращением строки month += 1; или строки month = month + 1;
+//            if (purchase == sumMax) {
+//            numberMonthSumMax = month;
+//            }
+//        }
+//        return numberMonthSumMax;
+
+	public int numberMonthSumMin(int[] purchases) {
+		int sumMin = getSumMin(purchases);
+		int month = 0;
+		int numberMonthSumMin = 0;
+		for (int purchase : purchases) {
+			month++;
+			if (sumMin == purchase) {
+				numberMonthSumMin = month;
+			}
+		}
+		return numberMonthSumMin;
+	}
+
+	public int getSumMin(int[] purchases) {
+		int sumMin = purchases[0];
+		for (int purchase : purchases) {
+			if (sumMin > purchase) {
+				sumMin = purchase;
+			}
+		}
+		return sumMin;
+	}
+
+
+	public int saleAboveSumAverage(int[] purchases) {
+		int average = 15;
+		int quantity = 0;
+		for (int purchase : purchases) {
+			if (average < purchase) {
+				quantity++;
+			}
+		}
+		return quantity;
+	}
+
+	public int saleBelowSumAverage(int[] purchases) {
+		int sumAverage = 15;
+		int quantity = 0;
+		for (int purchase : purchases) {
+			if (sumAverage > purchase) {
+				quantity++;
+			}
+		}
+		return quantity;
+	}
 }
-
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -6,40 +6,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
-    @Test
-    void calculateSumTotal() {
-        StatsService service = new StatsService();
-        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 180;
-        int actual = service.calculateSumTotal(purchases);
-        assertEquals(expected, actual);
-    }
+	@Test
+	void calculateSumTotal() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//
+//        чтобы много раз не указывать в других тестах строки
+//        StatsService service = new StatsService();
+//        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//        их можно вывести наверх (для всех тестов, где они нужны), указав под class StatsServiceTest {
+//        private StatsService service = new StatsService();
+//        private int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//        после чего эти строки можно удалить из тестов, т.к. они уже будут вверху
+//
+		int expected = 180;
+		int actual = service.calculateSumTotal(purchases);
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    void calculateSumAverage() {
-        StatsService service = new StatsService();
-        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 15;
-        int actual = service.calculateSumAverage(purchases);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    void findSumMax() {
-        StatsService service = new StatsService();
-        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 20;
-        int actual = service.findSumMax(purchases);
-        assertEquals(expected, actual);
-    }
+	@Test
+	void calculateSumAverage() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+		int expected = 15;
+		int actual = service.calculateSumAverage(purchases);
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    void findSumMin() {
-        StatsService service = new StatsService();
-        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 7;
-        int actual = service.findSumMin(purchases);
-        assertEquals(expected, actual);
-    }
 
+	@Test
+	void numberMonthSumMax() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+		int expected = 8;
+		int actual = service.numberMonthSumMax(purchases);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void numberMonthSumMin() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+		int expected = 9;
+		int actual = service.numberMonthSumMin(purchases);
+		assertEquals(expected, actual);
+	}
+
+
+	@Test
+	void quantityMonthsSaleAboveSumAverage() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+		int expected = 5;
+		int actual = service.saleAboveSumAverage(purchases);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void quantityMonthsSaleBelowSumAverage() {
+		StatsService service = new StatsService();
+		int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+		int expected = 5;
+		int actual = service.saleBelowSumAverage(purchases);
+		assertEquals(expected, actual);
+	}
 }
